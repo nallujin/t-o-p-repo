d7bdc98a2e6e12a650017f24d1126d7ed7aaef73
nallujin <benedictuspujagarfield@gmail.com>
2023-10-26 09:56:05 +0700

    Edited

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # My JavaScript learning repository
 > From:  [Codevolution | JavaScript Crash Course - Tutorial for Complete Beginners](https://youtu.be/XIOLqoPHCJ4?si=4oiOB0x_4k45Egke)
-> <img src="https://i.ytimg.com/vi/XIOLqoPHCJ4/maxresdefault.jpg">
-- [ ] Adding comments
-- [ ] Variables
+> 
+> [<img src="https://i.ytimg.com/vi/XIOLqoPHCJ4/maxresdefault.jpg">](https://youtu.be/XIOLqoPHCJ4?si=4oiOB0x_4k45Egke)
+- [x] Adding comments
+- [x] Variables
 - [ ] Data types
 - [ ] Operators
 - [ ] Type conversion
---
+++ main.js
@@ -0,0 +1,43 @@
+// This is a comment. This line is ignored
+// console.log("Hello from main.js");
+
+// Variables
+// let age = 25;
+// console.log(age);
+//
+// const salary =  0;
+// You cannot re-assign a const variable.
+// salary = 10; won't work
+
+// let sum = 0;
+// sum = 5;
+// console.log(sum);
+//
+// console.log(salary);
+
+const name = 'Vishwas';
+const language = "JavaScript";
+const channel = `Codevolution`;
+
+const total = 0;
+const PI = 3.14;
+
+const isPrimaryNumber = true;
+const isNewUser = false;
+
+let result;
+console.log(result);
+
+const res = null;
+
+const data = null;
+
+const person = {
+    firstName: "Bruce",
+    lastName: 'Wayne',
+    age: 30
+}
+
+console.log(person.firstName);
+const oddNumbers = [1,3,5,7,9];
+console.log(oddNumbers[0]);
