05dd3d9051073e187bde47aff0557c12e05a4308
nallujin <benedictuspujagarfield@gmail.com>
2024-05-27 23:24:05 +0700

    Clear the Searching algorithm task

--- prototypal-inheritance/searching-algo.js
+++ prototypal-inheritance/searching-algo.js
@@ -12,14 +12,19 @@ let head = {
 };
 
 let table = {
+  __proto__: head,
   pen: 3
 };
 
 let bed = {
+  __proto__: table,
   sheet: 1,
   pillow: 2
 };
 
 let pockets = {
+  __proto__: bed,
   money: 2000
-};//
+};
+
+console.log(pockets.glasses);
